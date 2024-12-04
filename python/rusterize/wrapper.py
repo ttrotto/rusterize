dc9ed544b81a7effd255dea308d4ec2cb2d284b8
ttrotto <armityle@outlook.it>
2024-12-03 23:14:46 -0800

    fixed check input logic

--- python/rusterize/wrapper.py
+++ python/rusterize/wrapper.py
@@ -1,17 +1,19 @@
-from typing import List, Optional, Union
+from typing import Optional, Tuple, Union
 
-from math import floor, ceil
-import geopandas as gpd
+from geopandas import GeoDataFrame
+import polars as pl
 import xarray as xr
+
+SUPPORTED_GEOM = {"Polygon", "Multipolygon"}
 
 
 class Rusterize:
     """ Fast geopandas rusterization into xarray.DataArray """
     def __init__(self,
-                 gdf: gpd.GeoDataFrame,
+                 gdf: GeoDataFrame,
                  field: Optional[str],
                  by: Optional[str],
-                 res: Union[int, float],
+                 res: Union[Tuple[int, ...], Tuple[float, ...]],
                  pixel_fn: str = "last",
                  background: Union[int, float] = 0):
         """
@@ -19,10 +21,10 @@ class Rusterize:
 
         Args:
             gdf: geopandas dataframe to rasterize
-            pixel_fn: pixel function to use, see fasterize manual for options
             field: field to rasterize
-            by: column to rasterize, assigns each unique value to a layer in the stack.
-                Mutually exclusive with field.
+            by: column to rasterize, assigns each unique value to a layer in the stack based on field.
+            res: tuple of (xres, yres) for rasterized data
+            pixel_fn: pixel function to use, see fasterize for options
             background: background value in final raster
 
         Returns:
@@ -33,57 +35,49 @@ class Rusterize:
         self.by = by
         self.res = res
         self.pixel_fn = pixel_fn
-        self.bakground = background
+        self.background = background
 
         # type checks
-        if not isinstance(self.gdf, gpd.GeoDataFrame):
+        if not isinstance(self.gdf, GeoDataFrame):
             raise TypeError("Must pass a valid geopandas dataframe.")
         if not isinstance(self.field, (str, type(None))):
             raise TypeError("Must pass a valid string to field.")
         if not isinstance(self.by, (str, type(None))):
             raise TypeError("Must pass a valid string to by.")
-        if not isinstance(self.res, (int, float)):
-            raise TypeError("Must pass a valid resolution type.")
+        if not isinstance(self.res, (Tuple[int, ...], Tuple[float, ...])):
+            raise TypeError("Must pass a valid resolution tuple.")
         if not isinstance(self.pixel_fn, str):
             raise TypeError("Must pass a valid string to pixel_fn.")
         if not isinstance(self.background, (int, float)):
             raise TypeError("Must pass a valid background type.")
 
         # value check
-        if self.res <= 0:
-            raise ValueError("Must pass a valid resolution value.")
+        if by and not field:
+            raise ValueError("If by is specified, field must also be specified.")
+        if len(self.res) < 2 or any(self.res) <= 0 or any(not isinstance(x, (int, float)) for x in self.res):
+            raise ValueError("Must pass valid resolution tuple values.")
         if self.pixel_fn not in ["sum", "first", "last", "min", "max", "count", "any"]:
             raise ValueError("pixel_fn must be one of sum, first, last, min, max, count, or any.")
 
         # geom type check
         geom_types = set(self.gdf.geom_type)
-        if len(geom_types) > 1 or "Polygon" not in geom_types:
-            raise NotImplementedError("Only Polygon geometry type is supported.")
+        if geom_types > SUPPORTED_GEOM or len(geom_types & SUPPORTED_GEOM) != 1:
+            raise NotImplementedError("Only Polygon and Multipolygon geometry types are supported.")
 
     def __call__(self):
-        if self.by:
-            return self.rusterize_by()
-        else:
-            return self.rusterize()
+        return self._rusterize()
 
-    def _pass_geom(self) -> List:
-        """
-        Geometries passthrough: distill geometry coordinates as list.
-        Needed because poor integration between geopandas and Rust.
-        """
-        return self.gdf.geometry.apply(lambda geom: list(geom.exterior.coords))
+    def _to_polars(self):
+        return pl.from_pandas(self.gdf)
 
-    def _pass_bounds(self) -> List:
-        """ Round down and up total bounds """
-        b = self.gdf.total_bounds
-        b[0], b[1] = floor(b[0]), floor(b[1])  # xmin, ymin
-        b[2], b[3] = ceil(b[2]), ceil(b[3])  # xmax, ymax
-        return b
+    # def _pass_bounds(self) -> List:
+    #     """ Round down and up total bounds """
+    #     b = self.gdf.total_bounds
+    #     b[0], b[1] = floor(b[0]), floor(b[1])  # xmin, ymin
+    #     b[2], b[3] = ceil(b[2]), ceil(b[3])  # xmax, ymax
+    #     return b
 
-    def rusterize(self) -> xr.DataArray:
-        """ Rasterize field """
-        crds = self._pass_geom()
+    def _rusterize(self) -> xr.DataArray:
+        pdf = self._to_polars()
 
-    def rusterize_by(self) -> xr.DataArray:
-        """ Rasterize by into stack """
-        pass
+
