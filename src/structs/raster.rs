9761a3f48b5f19207008c1ecb23ac5c38973d8dd
ttrotto <armityle@outlook.it>
2025-01-24 15:11:10 -0800

    construct coordinates directly from RasterInfo

--- src/structs/raster.rs
+++ src/structs/raster.rs
@@ -55,4 +55,24 @@ impl RasterInfo {
     pub fn build_raster(&self, bands: usize, background: f64) -> Array3<f64> {
         Array3::from_elem((bands, self.nrows, self.ncols), background)
     }
+
+    // construct coordinates for xarray (start from pixel's center)
+    pub fn make_coordinates<'py>(
+        &self,
+        py: Python<'py>,
+    ) -> (Bound<'py, PyArray1<f64>>, Bound<'py, PyArray1<f64>>) {
+        let y_coords = Array::range(
+            self.ymax - self.yres / 2.0,
+            self.ymax - self.nrows as f64 * self.yres - self.yres / 2.0,
+            -self.yres,
+        )
+        .into_pyarray_bound(py);
+        let x_coords = Array::range(
+            self.xmin + self.xres / 2.0,
+            self.xmin + self.ncols as f64 * self.xres + self.xres / 2.0,
+            self.xres,
+        )
+        .into_pyarray_bound(py);
+        (y_coords, x_coords)
+    }
 }
