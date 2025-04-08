9405f7f1ee59afb6ca6371207797c36c7fd22cbb
ttrotto <armityle@outlook.it>
2025-04-08 12:32:17 -0700

    rust fmt

--- src/rasterize.rs
+++ src/rasterize.rs
@@ -5,7 +5,7 @@ Rasterize a single (multi)polygon or (multi)linestring.
 use crate::edge_collection;
 use crate::pixel_functions::PixelFn;
 use crate::structs::{
-    edge::{less_by_x, less_by_ystart, EdgeCollection, PolyEdge},
+    edge::{EdgeCollection, PolyEdge, less_by_x, less_by_ystart},
     raster::RasterInfo,
 };
 
