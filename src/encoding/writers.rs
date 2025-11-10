ea8601ec686f56126eee005a430d2f251698a0b9
ttrotto <armityle@outlook.it>
2025-11-10 14:28:38 -0800

    writers to record pixels and build dense/sparse arrays

---
+++ src/encoding/writers.rs
@@ -0,0 +1,93 @@
+/* Handle how pixels are recorded depending on the output format */
+
+use crate::{
+    encoding::arrays::SparseArray,
+    rasterization::{pixel_functions::PixelFn, rusterize_impl::RasterizeConfig},
+};
+use ndarray::ArrayViewMut2;
+use num_traits::Num;
+
+pub trait PixelWriter<N: Num> {
+    fn write(&mut self, y: usize, x: usize, value: N, background: N);
+}
+
+pub struct DenseArrayWriter<'a, N> {
+    band: ArrayViewMut2<'a, N>,
+    pxfn: PixelFn<N>,
+}
+
+impl<'a, N: Num> PixelWriter<N> for DenseArrayWriter<'a, N> {
+    fn write(&mut self, y: usize, x: usize, value: N, background: N) {
+        (self.pxfn)(&mut self.band, y, x, value, background);
+    }
+}
+
+impl<'a, N: Num> DenseArrayWriter<'a, N> {
+    pub fn new(band: ArrayViewMut2<'a, N>, pxfn: PixelFn<N>) -> Self {
+        Self { band, pxfn }
+    }
+}
+
+// convert sparse writer into a sparse array
+pub trait ToSparseArray<N: Num> {
+    fn finish(self, config: RasterizeConfig<N>) -> SparseArray<N>;
+}
+
+pub struct SparseArrayWriter<N> {
+    pub band_name: String,
+    pub rows: Vec<usize>,
+    pub cols: Vec<usize>,
+    pub values: Vec<N>,
+}
+
+impl<N: Num> PixelWriter<N> for SparseArrayWriter<N> {
+    fn write(&mut self, y: usize, x: usize, value: N, _background: N) {
+        self.rows.push(y);
+        self.cols.push(x);
+        self.values.push(value);
+    }
+}
+
+impl<N: Num> ToSparseArray<N> for SparseArrayWriter<N> {
+    fn finish(self, config: RasterizeConfig<N>) -> SparseArray<N> {
+        let lengths = vec![self.values.len()];
+        let band_names = vec![self.band_name];
+        SparseArray::new(
+            band_names,
+            self.rows,
+            self.cols,
+            self.values,
+            lengths,
+            config,
+        )
+    }
+}
+
+impl<N: Num> ToSparseArray<N> for Vec<SparseArrayWriter<N>> {
+    fn finish(self, config: RasterizeConfig<N>) -> SparseArray<N> {
+        let (band_names, rows, cols, data, lengths) = self.into_iter().fold(
+            (Vec::new(), Vec::new(), Vec::new(), Vec::new(), Vec::new()),
+            |(mut band_names, mut rows, mut cols, mut data, mut lengths), writer| {
+                lengths.push(writer.values.len());
+                band_names.push(writer.band_name);
+                rows.extend(writer.rows);
+                cols.extend(writer.cols);
+                data.extend(writer.values);
+                (band_names, rows, cols, data, lengths)
+            },
+        );
+
+        SparseArray::new(band_names, rows, cols, data, lengths, config)
+    }
+}
+
+impl<N: Num> SparseArrayWriter<N> {
+    pub fn new(band_name: String) -> Self {
+        Self {
+            band_name,
+            rows: Vec::new(),
+            cols: Vec::new(),
+            values: Vec::new(),
+        }
+    }
+}
