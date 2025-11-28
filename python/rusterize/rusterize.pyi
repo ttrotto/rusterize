a135bec7df3ef3fdb3c0a5c53cbbdcb26cbfe51d
ttrotto <armityle@outlook.it>
2025-11-27 20:49:37 -0800

    fix: new return type

--- python/rusterize/rusterize.pyi
+++ python/rusterize/rusterize.pyi
@@ -16,11 +16,11 @@ def rusterize(
     burn: int | float | None = None,
     fun: str = "last",
     background: int | float | None = np.nan,
-    encoding: str = "dense",
+    encoding: str = "xarray",
     dtype: str = "float64",
-) -> DataArray | SparseArray:
+) -> DataArray | np.ndarray | SparseArray:
     """
-    Fast geopandas rasterization into xarray.DataArray
+    Fast geopandas rasterization in Rust.
 
     Args:
         :param gdf: geopandas dataframe to rasterize.
@@ -33,11 +33,11 @@ def rusterize(
         :param burn: burn a value onto the raster, mutually exclusive with `field`. Default is None.
         :param fun: pixel function to use. Available options are `sum`, `first`, `last`, `min`, `max`, `count`, or `any`. Default is `last`.
         :param background: background value in final raster. Default is np.nan.
-        :param encoding: return a dense array (burned geometries onto a raster) or a sparse array in COOrdinate format (coordinates and values of the rasterized geometries). Default is `dense`.
+        :param encoding: return a dense array (burned geometries onto a raster) or a sparse array in COOrdinate format (coordinates and values of the rasterized geometries). Available options are `xarray`, `numpy`, or `sparse`. The `xarray` encoding requires `xarray` and `rioxarray` to be installed. Default is `xarray`.
         :param dtype: specify the output dtype. Default is `float64`.
 
     Returns:
-        Rasterized xarray.DataArray in dense or COO sparse format.
+        xarray.DataArray, numpy.ndarray, or a sparse array in COO format.
 
     Notes:
         When any of `res`, `out_shape`, or `extent` is not provided, it is inferred from the other arguments when applicable.
@@ -47,8 +47,7 @@ def rusterize(
 
         If `field` is not in `gdf`, then a default `burn` value of 1 is rasterized.
 
-        A `None` value for `dtype` corresponds to the default of that dtype. An illegal value for a dtype will be replaced with the default of
-        that dtype. For example, a `background=np.nan` for `dtype="uint8"` will become `background=0`, where `0` is the default for `uint8`.
+        A `None` value for `dtype` corresponds to the default of that dtype. An illegal value for a dtype will be replaced with the default of that dtype. For example, a `background=np.nan` for `dtype="uint8"` will become `background=0`, where `0` is the default for `uint8`.
     """
 
 class SparseArray:
