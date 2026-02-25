aebe7940c8d0303229a6de0c6c70c870a2ffb5af
ttrotto <armityle@outlook.it>
2026-02-24 16:26:44 -0800

    infer bounding box for list or numpy input data

--- src/geo/raster.rs
+++ src/geo/raster.rs
@@ -1,5 +1,7 @@
 /* Structure to contain information on raster data */
 
+use geo::BoundingRect;
+use geo_types::{Geometry, Rect, coord};
 use num_traits::Num;
 use numpy::{
     IntoPyArray, PyArray1,
@@ -22,31 +24,22 @@ pub struct RasterInfo {
 
 #[derive(FromPyObject)]
 #[pyo3(from_item_all)]
-struct RawRasterInfo {
+pub struct RawRasterInfo {
     ncols: usize,
     nrows: usize,
     xmin: f64,
-    xmax: f64,
     ymin: f64,
+    xmax: f64,
     ymax: f64,
     xres: f64,
     yres: f64,
-    has_extent: bool,
+    with_user_extent: bool,
     tap: bool,
     epsg: Option<u16>,
 }
 
-impl<'py> FromPyObject<'py> for RasterInfo {
-    fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
-        let raw: RawRasterInfo = ob.extract()?;
-        let info = RasterInfo::from(raw);
-        Ok(info)
-    }
-}
-
 impl RasterInfo {
-    #[inline]
-    fn from(raw: RawRasterInfo) -> Self {
+    pub fn from(raw: RawRasterInfo, geoms: &[Geometry<f64>]) -> Self {
         let mut info = RasterInfo {
             ncols: raw.ncols,
             nrows: raw.nrows,
@@ -59,11 +52,36 @@ impl RasterInfo {
             epsg: raw.epsg,
         };
 
+        if info.xmin.is_infinite() {
+            // list or numpy.ndarray do not carry bounding information
+            let bounds = geoms.iter().fold(None, |acc, geom| {
+                let bounds = geom.bounding_rect();
+
+                match (acc, bounds) {
+                    (None, None) => None,
+                    (None, Some(r)) | (Some(r), None) => Some(r),
+                    (Some(r1), Some(r2)) => Some(Rect::new(
+                        coord! { x: r1.min().x.min(r2.min().x), y: r1.min().y.min(r2.min().y) },
+                        coord! { x: r1.max().x.max(r2.max().x), y: r1.max().y.max(r2.max().y) },
+                    )),
+                }
+            });
+
+            if let Some(b) = bounds {
+                info.xmin = b.min().x;
+                info.ymin = b.min().y;
+                info.xmax = b.max().x;
+                info.ymax = b.max().y;
+            } else {
+                panic!("Cannot infer bounding box from geometry.")
+            }
+        }
+
         let has_res = info.xres != 0.0;
         let has_shape = info.nrows != 0;
 
         // extent by half pixel if custom extent not provided
-        if !raw.has_extent && !raw.tap && has_res {
+        if !raw.with_user_extent && !raw.tap && has_res {
             info.xmin -= info.xres / 2.0;
             info.xmax += info.xres / 2.0;
             info.ymin -= info.yres / 2.0;
@@ -71,7 +89,7 @@ impl RasterInfo {
         }
 
         if !has_res {
-            info.resolution();
+            info.assign_resolution();
         } else if raw.tap && has_res {
             info.xmin = (info.xmin / info.xres).floor() * info.xres;
             info.xmax = (info.xmax / info.xres).ceil() * info.xres;
@@ -80,20 +98,20 @@ impl RasterInfo {
         }
 
         if !has_shape {
-            info.shape();
+            info.assign_shape();
         }
 
         info
     }
 
     #[inline]
-    fn shape(&mut self) {
+    fn assign_shape(&mut self) {
         self.nrows = (0.5 + (self.ymax - self.ymin) / self.yres) as usize;
         self.ncols = (0.5 + (self.xmax - self.xmin) / self.xres) as usize
     }
 
     #[inline]
-    fn resolution(&mut self) {
+    fn assign_resolution(&mut self) {
         self.xres = (self.xmax - self.xmin) / self.ncols as f64;
         self.yres = (self.ymax - self.ymin) / self.nrows as f64;
     }
