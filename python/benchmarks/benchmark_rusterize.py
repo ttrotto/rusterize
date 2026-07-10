e4fe676286388494c67da602e2c3bcb060a7cbc5
ttrotto <armityle@outlook.it>
2026-07-10 14:30:08 -0700

    move benchnmarks

--- python/benchmarks/benchmark_rusterize.py
+++ python/benchmarks/benchmark_rusterize.py
@@ -9,13 +9,13 @@ from rusterize import rusterize
 import rioxarray
 
 # POLYGONS (~468MB)
-url = "https://ftp.maps.canada.ca/pub/nrcan_rncan/vector/canvec/shp/Hydro/canvec_50K_BC_Hydro_shp.zip"
-response = requests.get(url)
-
-with zipfile.ZipFile(BytesIO(response.content), "r") as zip_ref:
-    for file_name in zip_ref.namelist():
-        if "canvec_50K_BC_Hydro/waterbody_2" in file_name:
-            zip_ref.extract(file_name)
+if not os.path.exists("canvec_50K_BC_Hydro/waterbody_2.shp"):
+    url = "https://ftp.maps.canada.ca/pub/nrcan_rncan/vector/canvec/shp/Hydro/canvec_50K_BC_Hydro_shp.zip"
+    response = requests.get(url)
+    with zipfile.ZipFile(BytesIO(response.content), "r") as zip_ref:
+        for file_name in zip_ref.namelist():
+            if "canvec_50K_BC_Hydro/waterbody_2" in file_name:
+                zip_ref.extract(file_name)
 
 
 water_large = read_dataframe("canvec_50K_BC_Hydro/waterbody_2.shp")
@@ -23,11 +23,11 @@ water_small = water_large.iloc[:1000, :]
 
 
 # LINESTRINGS (~900 MB)
-url = "https://www12.statcan.gc.ca/census-recensement/2011/geo/RNF-FRR/files-fichiers/lrnf000r25p_e.zip"
-response = requests.get(url)
-
-with zipfile.ZipFile(BytesIO(response.content), "r") as zip_ref:
-    zip_ref.extractall()
+if not os.path.exists("lrnf000r25p_e/lrnf000r25p_e.gpkg"):
+    url = "https://www12.statcan.gc.ca/census-recensement/2011/geo/RNF-FRR/files-fichiers/lrnf000r25p_e.zip"
+    response = requests.get(url)
+    with zipfile.ZipFile(BytesIO(response.content), "r") as zip_ref:
+        zip_ref.extractall()
 
 roads = read_dataframe("lrnf000r25p_e/lrnf000r25p_e.gpkg")
 
